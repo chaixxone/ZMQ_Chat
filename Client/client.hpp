f10aa86b5aa24d06c43b7bc4864b77ec56c7a91a
MaximKanevskiy <145599322+MaximKanevskiy@users.noreply.github.com>
2024-07-10 16:12:39 +0300

    upd: client.cpp - HasRequestToChat, Reply, GetChatId methods added;
    For accepting/refusing entering the chat created Reply method; to check if client has that request - HasRequestToChat. For sending message we needed GetChatId for getting client's current chat id.

--- Client/client.hpp
+++ Client/client.hpp
@@ -1,14 +1,18 @@
 #pragma once
 #include "client_interface.hpp"
 #include <thread>
+#include <mutex>
 
 class Client : IClient
 {
 public:
     Client(std::string endpoint, std::string identity);
     ~Client();
-    void RequestToCreateChat(std::string& clients) override;
+    void RequestToCreateChat(std::string& clients, std::string& chatId) override;
     void SendMessageToChat(const std::string& messageStr, const std::string& actionStr = "send_message") override;
+    bool HasRequestToChat() const;
+    void Reply(const std::string& reply);
+    size_t GetChatId() const;
 
 private:
     void _receiveMessage();
@@ -16,6 +20,8 @@ private:
     zmq::socket_t _socket;
     std::string _identity;
     std::thread _receiver;
-    uint16_t _chatId;
+    size_t _chatId;
     bool _isInChat;
+    bool _hasRequestToChat;
+    std::mutex _mutex;
 };
