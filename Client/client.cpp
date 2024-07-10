af7ba4397fafb11d7e7f6560ace28728fd22f2d0
MaximKanevskiy <145599322+MaximKanevskiy@users.noreply.github.com>
2024-07-10 16:08:49 +0300

    fix, upd: client.cpp - chat id formatting issue , GetChatId method, chat id assignment
    Fixed getting chat id from stdin. So far the normal chat structure is occupied by a duct tape in the form of assigning the _chatId field to the chat client-host so they can receive messages from other clients in the spicific chat

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,13 +1,13 @@
-﻿#include "client.hpp"
+#include "client.hpp"
 #include <iostream>
 
-Client::Client(std::string endpoint, std::string identity) : _context(1), _socket(_context, zmq::socket_type::dealer), _identity(identity), _isInChat(false)
+Client::Client(std::string endpoint, std::string identity)
+    : _context(1), _socket(_context, zmq::socket_type::dealer), _identity(identity), _isInChat(false), _hasRequestToChat(false)
 {
     _socket.set(zmq::sockopt::routing_id, _identity);
     _socket.connect(endpoint);
 
     std::string connection = "!connect!";
-
     SendMessageToChat(connection, connection);
 
     _receiver = std::thread(&Client::_receiveMessage, this);
@@ -21,52 +21,62 @@ Client::~Client()
     }
 }
 
-// ------------------------------------Ready-to-use------------------------------------------
+size_t Client::GetChatId() const
+{
+    return _chatId;
+}
+
+bool Client::HasRequestToChat() const
+{
+    return _hasRequestToChat;
+}
+
 void Client::SendMessageToChat(const std::string& messageStr, const std::string& actionStr)
 {
     zmq::message_t action(actionStr);
     zmq::message_t message(messageStr);
-    _socket.send(action, zmq::send_flags::sndmore);
-    _socket.send(message, zmq::send_flags::none);
+    bool result = _socket.send(action, zmq::send_flags::sndmore) && _socket.send(message, zmq::send_flags::none);
+
+    if (!result)
+    {
+        std::cerr << "Failed to send message to chat." << std::endl;
+    }
 }
 
-void Client::RequestToCreateChat(std::string& clients)
+void Client::RequestToCreateChat(std::string& clients, std::string& chatId)
 {
-    if (clients.back() == ' ') clients.pop_back();
-    SendMessageToChat(clients, "create_chat");
+    if (!clients.empty() && clients.back() == ' ') clients.pop_back();
+    std::cout << "I am requesting: " << clients << ", to create chat " << chatId << std::endl;
+    std::string chatInfo = "create_chat:" + chatId;
+    SendMessageToChat(clients, chatInfo);
+    _chatId = static_cast<size_t>(stoi(chatId));
 }
-// ------------------------------------------------------------------------------------------
 
-void Client::_receiveMessage() 
+void Client::_receiveMessage()
 {
-    while (true) 
+    while (true)
     {
         zmq::message_t action, data;
-        _socket.recv(action);
-        _socket.recv(data);
+        _socket.recv(action, zmq::recv_flags::none);
+        _socket.recv(data, zmq::recv_flags::none);
+
         std::string actionStr = action.to_string();
         std::string dataStr = data.to_string();
+        std::cout << actionStr << " " << _isInChat << std::endl;
 
-        // -------------Have-to-be-done-with-server's-askClients-method----------------------
-        if (actionStr == "create_chat" && !_isInChat)
+        std::unique_lock<std::mutex> lock(_mutex);
+        if (actionStr.substr(0, 12) == "create_chat:" && !_isInChat)
         {
-            std::cout << "[Server] Do you wish to create chat with " << data << "? (y/n)" << std::endl;
-            std::string userInput;
-            std::cin >> userInput;
-
-            if (userInput == "y")
-            {
-                SendMessageToChat("yes", "accept_create_chat");
-            }
-            else 
-            {
-                SendMessageToChat("no", "decline_create_chat");
-            }
+            _chatId = static_cast<size_t>(stoi(actionStr.substr(12)));
+            std::cout << "[" << _identity << "]" << " I am invited to chat " << _chatId << std::endl;
+            lock.unlock();
+            _hasRequestToChat = true;
+            std::cout << "[Server] Do you wish to create chat with " << dataStr << "? (y/n)" << std::endl;
+            lock.lock();
         }
-        // -----------------------------------------------------------------------------------
         else if (actionStr == "new_chat")
         {
-            _chatId = stoi(dataStr);
+            _chatId = std::stoi(dataStr);
             std::cout << "[Server] Now you are in chat with id=" << dataStr << std::endl;
             _isInChat = true;
         }
@@ -79,4 +89,20 @@ void Client::_receiveMessage()
             std::cout << "Error: unknown action!" << std::endl;
         }
     }
+}
+
+void Client::Reply(const std::string& reply)
+{
+    if (reply == "y")
+    {
+        std::cout << "accepted!" << std::endl;
+        SendMessageToChat(std::to_string(_chatId), "accept_create_chat");
+    }
+    else
+    {
+        SendMessageToChat(std::to_string(_chatId), "decline_create_chat");
+        _chatId = NULL;
+    }
+
+    _hasRequestToChat = false;
 }
