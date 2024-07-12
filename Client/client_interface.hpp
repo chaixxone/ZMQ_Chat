f61bb979af32474b570b5d13b25f177d68dafea8
MaximKanevskiy <145599322+MaximKanevskiy@users.noreply.github.com>
2024-07-12 08:59:15 +0300

    change: cli_interface.hpp - message parameter now is not const

--- Client/client_interface.hpp
+++ Client/client_interface.hpp
@@ -5,6 +5,6 @@
 class IClient
 {
 public:
-	virtual void RequestToCreateChat(std::string& clients) = 0;
-	virtual void SendMessageToChat(const std::string& messageStr, const std::string& actionStr) = 0;
+	virtual void RequestToCreateChat(std::string& clients, std::string& chatId) = 0;
+	virtual void SendMessageToChat(std::string& messageStr, const std::string& actionStr) = 0;
 };
