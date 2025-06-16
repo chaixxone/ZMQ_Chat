1588a5a0c122033ad70823bf41c754dc383c7d9a
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-06-16 13:17:20 +0300

    refactor: move function and enum to unnamed namespace

--- Server/server.cpp
+++ Server/server.cpp
@@ -2,38 +2,41 @@
 #include <iostream>
 #include <sstream>
 
-enum class Action
+namespace 
 {
-    Connect,
-    SendMessage,
-    CreateChat,
-    AcceptCreateChat,
-    Unknown
-};
-
-static Action stringToAction(const std::string& actionStr)
-{
-    const short CREATE_CHAT_PREFX_LENGTH = 12;
-
-    static const std::unordered_map<std::string, Action> actionMap = {
-        {"!connect!", Action::Connect},
-        {"send_message", Action::SendMessage},
-        {"create_chat", Action::CreateChat},
-        {"accept_create_chat", Action::AcceptCreateChat}
+    enum class Action
+    {
+        Connect,
+        SendMessage,
+        CreateChat,
+        AcceptCreateChat,
+        Unknown
     };
 
-    auto it = actionMap.find(actionStr);
-
-    if (it != actionMap.end())
+    Action stringToAction(const std::string& actionStr)
     {
-        return it->second;
-    }
-    else if (actionStr.substr(0, CREATE_CHAT_PREFX_LENGTH) == "create_chat:")
-    {
-        return Action::CreateChat;
-    }
+        const short CREATE_CHAT_PREFX_LENGTH = 12;
 
-    return Action::Unknown;
+        static const std::unordered_map<std::string, Action> actionMap = {
+            {"!connect!", Action::Connect},
+            {"send_message", Action::SendMessage},
+            {"create_chat", Action::CreateChat},
+            {"accept_create_chat", Action::AcceptCreateChat}
+        };
+
+        auto it = actionMap.find(actionStr);
+
+        if (it != actionMap.end())
+        {
+            return it->second;
+        }
+        else if (actionStr.substr(0, CREATE_CHAT_PREFX_LENGTH) == "create_chat:")
+        {
+            return Action::CreateChat;
+        }
+
+        return Action::Unknown;
+    }
 }
 
 Server::Server(std::string binding) : _context(1), _socket(_context, zmq::socket_type::router)
