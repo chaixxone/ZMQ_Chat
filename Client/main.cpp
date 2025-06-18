da5ca54ac8d5ff399f8339ed2789159df60530ba
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-06-18 17:38:39 +0300

    feat: define trim function for identity

--- Client/main.cpp
+++ Client/main.cpp
@@ -2,6 +2,29 @@
 #include <limits>
 #include "client.hpp"
 
+namespace
+{
+    std::string trim(const std::string& s)
+    {
+        size_t lindex = 0;
+        size_t size = s.size();
+
+        while (lindex < size && s[lindex] == ' ')
+        {
+            lindex++;
+        }
+
+        size_t rindex = lindex;
+
+        while (rindex < size && s[rindex] != ' ')
+        {
+            rindex++;
+        }
+
+        return std::string(s.begin() + lindex, s.begin() + rindex);
+    }
+}
+
 int main(int argc, char** argv)
 {
     std::string host, self;
