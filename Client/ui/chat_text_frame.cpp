8d7429dbe257b9308af8aba6cf37a41e8ce0d375
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-06-21 17:05:01 +0300

    feat: define remove message private slot

--- Client/ui/chat_text_frame.cpp
+++ Client/ui/chat_text_frame.cpp
@@ -19,4 +19,34 @@ void ChatTextFrame::AddMessage(Message* message)
 	_messages->addItem(messageItem);
 }
 
+void ChatTextFrame::RemoveMessage(int messageId)
+{
+	size_t messages = _messages->count();
+	size_t left = 0;
+	size_t right = messages - 1;
+	size_t index = 0;
+
+	while (left <= right)
+	{
+		size_t middle = (left + right) / 2;
+		size_t messageIdAtMiddle = qvariant_cast<Message*>(_messages->item(middle)->data(Qt::UserRole))->GetId();
+
+		if (messageIdAtMiddle < middle)
+		{
+			right = middle;
+		}
+		else if (messageId > middle)
+		{
+			left = middle;
+		}
+		else
+		{
+			index = middle;
+			break;
+		}
+	}
+
+	_messages->removeItemWidget(_messages->item(index));
+}
+
 ChatTextFrame::~ChatTextFrame() {}
