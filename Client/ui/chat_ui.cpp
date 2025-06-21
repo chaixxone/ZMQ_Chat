1d365b41b2bf96ff5bb2750323de16fa1897c295
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-06-21 18:12:52 +0300

    feat: define chat ui dummy constructor

---
+++ Client/ui/chat_ui.cpp
@@ -0,0 +1,58 @@
+#include <chat_ui.hpp>
+#include <chat_text_frame.hpp>
+#include <chat_text_line.hpp>
+
+using namespace UI;
+
+ChatUI::ChatUI(QWidget* parent) : 
+	QMainWindow(parent), 
+	_pages(new QStackedWidget(this)), 
+	_loginPage(new QWidget), 
+	_mainPage(new QWidget)
+{
+	_pages->addWidget(_loginPage);
+	_pages->addWidget(_mainPage);
+	// TODO: remove current index setting
+	_pages->setCurrentIndex(1);
+	setCentralWidget(_pages);
+
+	// -----------------------------------
+	// left side panel widgets
+	auto nameLineEdit = new QLineEdit;
+	auto userComboBox = new QComboBox;
+	auto chatIdComboBox = new QComboBox;
+	auto userChatIdComboBox = new QComboBox;
+
+	auto vSidePanelLayout = new QVBoxLayout;
+	vSidePanelLayout->addWidget(nameLineEdit);
+	vSidePanelLayout->addWidget(userComboBox);
+	vSidePanelLayout->addWidget(chatIdComboBox);
+	vSidePanelLayout->addWidget(userChatIdComboBox);
+	// main space
+	auto chat = new ChatTextFrame;
+	// TODO: remove dummy message
+	QString dummyText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"
+		"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim"
+		"ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip"
+		"ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate"
+		"velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat"
+		"cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	auto message = new Message(564755748ull, "chaixxone", dummyText, chat);	
+	chat->AddMessage(message);
+	auto messageTextBar = new ChatTextLine(300, 25);
+
+	auto vMainSpaceLayout = new QVBoxLayout;
+	vMainSpaceLayout->addWidget(chat);
+	vMainSpaceLayout->addWidget(messageTextBar);
+	// -----------------------------------
+
+	auto hLayoutMainPage = new QHBoxLayout;
+	hLayoutMainPage->addLayout(vSidePanelLayout);
+	hLayoutMainPage->addLayout(vMainSpaceLayout);
+
+	_mainPage->setLayout(hLayoutMainPage);
+
+	// TODO: connect widgets
+}
+
+ChatUI::~ChatUI() {}
