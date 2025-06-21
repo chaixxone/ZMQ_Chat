fa9c9d0be775887ab93fd006e6379c17ba2edc01
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-06-21 18:11:58 +0300

    feat: define message class

--- Client/ui/message.cpp
+++ Client/ui/message.cpp
@@ -1,1 +1,27 @@
 #include <message.hpp>
+
+using namespace UI;
+
+Message::Message(size_t id, QString author, QString text, QWidget* parent) :
+	QWidget(parent), _id(id), _author(author), _content(new QTextEdit(text))
+{
+	auto layout = new QVBoxLayout;
+	layout->addWidget(new QLabel(_author));
+	_content->setReadOnly(true);
+	_content->setStyleSheet("border: none; background: transparent;");
+	_content->setMinimumHeight(50);
+	layout->addWidget(_content);
+	setLayout(layout);
+}
+
+Message::~Message() {}
+
+size_t Message::GetId() const noexcept
+{
+	return _id;
+}
+
+QString Message::GetContent() const noexcept
+{
+	return _content->toPlainText();
+}
