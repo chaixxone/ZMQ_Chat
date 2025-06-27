46de0d9364adcb48de25b2401847d063b2b35f41
vg <maxkanevskiy430@yandex.ru>
2025-06-27 16:25:21 +0300

    feat: add message queue field to client

--- Client/include/client.hpp
+++ Client/include/client.hpp
@@ -1,11 +1,12 @@
 #pragma once
 #include "client_interface.hpp"
+#include "message_queue.hpp"
 #include <thread>
 
 class Client : public IClient
 {
 public:
-    Client(std::string endpoint, std::string identity);
+    Client(std::string endpoint, std::string identity, std::shared_ptr<MessageQueue> messageQueue);
     ~Client();
     void RequestToCreateChat(std::string& clients, const std::string& chatId) override;
     void SendMessageToChat(std::string& messageStr, const std::string& actionStr = "send_message") override;
@@ -22,6 +23,7 @@ private:
     std::string _identity;
     zmq::socket_t _socket;
     std::thread _receiver;
+    std::shared_ptr<MessageQueue> _messageQueue;
     zmq::context_t _context;
     size_t _chatId;
     bool _isInChat;
