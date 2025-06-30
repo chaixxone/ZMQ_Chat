4de5b65bb1a0e53f2a13247f8ad6a167a676daa6
vg <maxkanevskiy430@yandex.ru>
2025-06-30 12:31:49 +0300

    feat: declare qt message observer class

---
+++ Client/include/qt_message_observer.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+
+#include <client_interface.hpp>
+#include <message_observer_interface.hpp>
+
+class QtMessageObserver : public IMessageObserver
+{
+public:
+	QtMessageObserver(std::shared_ptr<IClient> client);
+	~QtMessageObserver();
+
+	void Update() override;
+};
