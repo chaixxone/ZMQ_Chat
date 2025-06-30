0a375b293c69d3e35c4b842ffb32f3d66e419123
vg <maxkanevskiy430@yandex.ru>
2025-06-30 13:20:12 +0300

    feat: inherit enable shared from this class

--- Client/include/qt_message_observer.hpp
+++ Client/include/qt_message_observer.hpp
@@ -5,7 +5,7 @@
 #include <client.hpp>
 #include <message_observer_interface.hpp>
 
-class QtMessageObserver : public IMessageObserver
+class QtMessageObserver : public IMessageObserver, public std::enable_shared_from_this<QtMessageObserver>
 {
 public:
 	QtMessageObserver(std::shared_ptr<Client> client);
