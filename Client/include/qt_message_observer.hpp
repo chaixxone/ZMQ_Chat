1c8cfcca841ca223cba7226ca699a5a3346399f8
vg <maxkanevskiy430@yandex.ru>
2025-07-01 11:58:15 +0300

    feat: make qt message observer a q object class

--- Client/include/qt_message_observer.hpp
+++ Client/include/qt_message_observer.hpp
@@ -2,11 +2,15 @@
 
 #include <memory>
 
+#include <QObject>
+
 #include <client.hpp>
 #include <message_observer_interface.hpp>
 
-class QtMessageObserver : public IMessageObserver, public std::enable_shared_from_this<QtMessageObserver>
+class QtMessageObserver : public IMessageObserver, public std::enable_shared_from_this<QtMessageObserver>, public QObject
 {
+	Q_OBJECT
+
 public:
 	QtMessageObserver();
 	~QtMessageObserver();
