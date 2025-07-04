c059c46ced388aba2f1e66246c46372a7e044362
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-07-04 15:06:18 +0300

    fix: cover size_t message id by std optional, store nullopt if no message id present

--- Client/include/message_view.hpp
+++ Client/include/message_view.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <string>
+#include <optional>
 #include <utils/client_actions.hpp>
 
 struct MessageView
 {
 	std::string Author;
 	std::string Content;
-	size_t ID;
+	std::optional<size_t> ID;
 	int ChatID;
 	Utils::Action Action;
 };
