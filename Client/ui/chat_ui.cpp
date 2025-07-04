452941c19d5f3714904b934b1c1d2378005af1c5
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-07-04 14:17:47 +0300

    refactor: do not convert message view id to size_t

--- Client/ui/chat_ui.cpp
+++ Client/ui/chat_ui.cpp
@@ -58,7 +58,7 @@ ChatUI::ChatUI(std::shared_ptr<Client> client, std::shared_ptr<QtMessageObserver
 	// TODO: connect widgets
 	connect(_messageObserver.get(), &QtMessageObserver::IncomingMessage, chat, [this, chat](const MessageView& messageView) {
 		auto message = new Message(
-			std::stoull(messageView.ID),
+			messageView.ID,
 			QString::fromStdString(messageView.Author),
 			QString::fromStdString(messageView.Content),
 			chat
