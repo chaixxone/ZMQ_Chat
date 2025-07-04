f1fc184bf83fa8a7bb08f60019fa80abf957c9e3
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-07-04 14:31:43 +0300

    fix: add noexcept keyword to set current chat method

--- Client/ui/chat_text_frame.cpp
+++ Client/ui/chat_text_frame.cpp
@@ -52,7 +52,7 @@ void ChatTextFrame::RemoveMessage(int messageId)
 
 ChatTextFrame::~ChatTextFrame() {}
 
-void ChatTextFrame::SetCurrentChat(const QString& chatIdStr)
+void ChatTextFrame::SetCurrentChat(const QString& chatIdStr) noexcept
 {
 	_currentChat = chatIdStr.toInt();
 }
