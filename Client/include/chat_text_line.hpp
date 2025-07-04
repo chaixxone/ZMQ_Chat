081ec9c2abce5870a8aeb4811343f7e9cfb0ca88
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-07-04 13:54:53 +0300

    feat: emit sended text signal on shift + enter key combination

--- Client/include/chat_text_line.hpp
+++ Client/include/chat_text_line.hpp
@@ -10,10 +10,16 @@ namespace UI
 	public:
 		explicit ChatTextLine(int maxWidth, int minHeight, QWidget* parent = nullptr);
 
+	protected:
+		void keyPressEvent(QKeyEvent* event) override;
+
 	private:
 		int m_maxWidth;
 		int m_minHeight;
 		static const int m_maxVisibleLines = 5;
+
+	signals:
+		void SendedText(const QString& text);
 
 	private slots:
 		void AdjustHeight();
--- Client/ui/chat_text_line.cpp
+++ Client/ui/chat_text_line.cpp
@@ -1,4 +1,5 @@
 #include "chat_text_line.hpp"
+#include <QKeyEvent>
 
 UI::ChatTextLine::ChatTextLine(int maxWidth, int minHeight, QWidget* parent) :
 	QTextEdit(parent), m_maxWidth(maxWidth), m_minHeight(minHeight)
@@ -13,4 +14,23 @@ void UI::ChatTextLine::AdjustHeight()
 	int lines = document()->lineCount();
 	int heightMultiplier = qMin(lines, m_maxVisibleLines);
 	resize(m_maxWidth, m_minHeight * heightMultiplier);
+}
+
+void UI::ChatTextLine::keyPressEvent(QKeyEvent* event)
+{
+	QTextEdit::keyPressEvent(event);
+
+	if (event->key() == Qt::Key::Key_Return && (event->modifiers() & Qt::KeyboardModifier::ControlModifier))
+	{
+		qDebug() << "ctrl + enter";
+
+		QString text = document()->toPlainText().trimmed();
+
+		if (!text.isEmpty())
+		{
+			document()->clear();
+			emit SendedText(text);
+			return;
+		}
+	}
 }
