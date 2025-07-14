50204f591f428fcd511b31dee2149e2d78256f69
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-07-14 12:52:16 +0300

    fix: popup combo box contents when delay timer sends signal timeout

--- Client/ui/popup_signal_emitting_q_combo_box.cpp
+++ Client/ui/popup_signal_emitting_q_combo_box.cpp
@@ -1,9 +1,18 @@
 #include <popup_signal_emitting_q_combo_box.hpp>
 
-UI::PopUpSignalEmittingQComboBox::PopUpSignalEmittingQComboBox(QWidget* parent) : QComboBox(parent) {}
+UI::PopUpSignalEmittingQComboBox::PopUpSignalEmittingQComboBox(QWidget* parent) :
+	QComboBox(parent),
+	_popupDelayTimer(new QTimer(this))
+{
+	_popupDelayTimer->setSingleShot(true);
+	_popupDelayTimer->setInterval(std::chrono::milliseconds(50));	
+	connect(this, &PopUpSignalEmittingQComboBox::PoppedUp, _popupDelayTimer, QOverload<>::of(&QTimer::start));
+	connect(_popupDelayTimer, &QTimer::timeout, this, [this]() {
+		QComboBox::showPopup();
+	});
+}
 
 void UI::PopUpSignalEmittingQComboBox::showPopup()
 {
 	emit PoppedUp();
-	QComboBox::showPopup();
 }
