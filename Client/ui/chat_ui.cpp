7a4538cfee4b1cc299a0dba863fb9490a4a4c9c3
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-07-18 21:38:07 +0300

    feat: reply to chat invitation on invitation processed signal

--- Client/ui/chat_ui.cpp
+++ Client/ui/chat_ui.cpp
@@ -303,6 +303,10 @@ ChatUI::ChatUI(std::shared_ptr<Client> client, std::shared_ptr<QtMessageObserver
 	connect(_messageObserver.get(), &QtMessageObserver::CreateChat, _noticeBox, [this](const MessageView& messageView) {
 		_noticeBox->AddNotification(messageView);
 	});
+	connect(_noticeBox, &NoticeBox::InvitationProcessed, [this](int chatId, bool isAccepted) {
+		std::string dummyInviteResponseString = isAccepted ? "yes" : "no";
+		_client->ReplyChatInvite(dummyInviteResponseString);
+	});
 
 	connect(messageTextBar, &ChatTextLine::SendedText, [this, chat](const QString& text) {
 		std::string stdText = text.toStdString();
