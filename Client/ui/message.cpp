8f3b5740637ea7fe0fefd80432f301aff6edd00d
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-07-20 18:21:11 +0300

    feat: adjust size of a Message widget depending on QTextDocument size

--- Client/ui/message.cpp
+++ Client/ui/message.cpp
@@ -5,15 +5,23 @@ using namespace UI;
 Message::Message(size_t id, QString author, QString text, QWidget* parent) :
 	QWidget(parent), _id(id), _author(std::move(author)), _content(new QTextEdit(std::move(text)))
 {
-	auto layout = new QVBoxLayout;
-	layout->addWidget(new QLabel(_author));
 	_content->setReadOnly(true);
 	_content->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	_content->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	_content->setStyleSheet("border: none; background: transparent;");
-	_content->setMinimumHeight(50);
+	_content->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+	auto layout = new QVBoxLayout;
+	layout->addWidget(new QLabel(_author));
 	layout->addWidget(_content);
 	setLayout(layout);
+
+	QTextDocument* doc = _content->document();
+	int documentWidth = _content->viewport()->width();
+	doc->setTextWidth(documentWidth);
+	int documentHeight = doc->size().height();
+	_content->setFixedHeight(documentHeight);
+	adjustSize();
 }
 
 Message::~Message() {}
