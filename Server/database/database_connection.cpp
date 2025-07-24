5666fcf1588b1579ead4f1939e874cf87abe7c63
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-07-24 12:46:05 +0300

    feat: define DatabaseConnection ctor

--- Server/database/database_connection.cpp
+++ Server/database/database_connection.cpp
@@ -1,1 +1,8 @@
 #include <database_connection.hpp>
+
+DatabaseConnection::DatabaseConnection(std::string host, std::string user, std::string password, std::string schema) : 
+	_driver(get_driver_instance())
+{
+	_connection = std::unique_ptr<sql::Connection>(_driver->connect(std::move(host), std::move(user), std::move(password)));
+	_connection->setSchema(std::move(schema));
+}
