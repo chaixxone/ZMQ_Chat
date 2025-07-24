ba93b3d5e478b3e103f6304870d97b716cb3060d
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-07-24 13:53:59 +0300

    feat: declare user authorization methods

--- Server/include/database_connection.hpp
+++ Server/include/database_connection.hpp
@@ -10,7 +10,14 @@ class DatabaseConnection
 public:
 	DatabaseConnection(std::string host, std::string user, std::string password, std::string schema);
 
+	[[nodiscard]] bool RegisterUser(const std::string& identity, const std::string& password) const;
+	[[nodiscard]] bool AuthorizeUser(const std::string& identity, const std::string& password) const;
+
 private:
 	sql::Driver* _driver;
 	std::unique_ptr<sql::Connection> _connection;
+
+	bool DoesUserExist(const std::string& identity);
+	std::string HashPassword(const std::string& password);
+	std::string GetPasswordHash(const std::string& identity);
 };
