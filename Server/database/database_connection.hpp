81bd3de7efcc8117f1b3d4fcfeb8ee0fc8777a63
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-07-24 17:22:34 +0300

    feat: create concrete db connection class in db_conn.dll release library

---
+++ Server/database/database_connection.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+class __declspec(dllexport) DatabaseConnection
+{
+public:
+	virtual ~DatabaseConnection() = default;
+
+	virtual bool RegisterUser(const char* identity, const char* password) const = 0;
+	virtual bool AuthorizeUser(const char* identity, const char* password) const = 0;
+
+private:
+	virtual bool DoesUserExist(const char* identity) = 0;
+	virtual const char* HashPassword(const char* password) = 0;
+	virtual const char* GetPasswordHash(const char* identity) = 0;
+};
+
+extern "C"
+__declspec(dllexport)
+DatabaseConnection* CreateDatabaseConnection(const char* host, const char* user, const char* password, const char* schema);
