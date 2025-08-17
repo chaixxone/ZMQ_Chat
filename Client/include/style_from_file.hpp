26e20ccc1246ab4298f749bdd7203c4b4d43c5de
MaximKanevskiy <maxkanevskiy430@yandex.ru>
2025-08-17 16:18:59 +0300

    feat: add file with function setStyleFromFile

---
+++ Client/include/style_from_file.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <QString>
+#include <QWidget>
+#include <QFile>
+
+namespace UI
+{
+	void setStyleFromFile(QWidget* widget, const QString& path)
+	{
+		QFile styleFile(path);
+
+		if (!styleFile.open(QFile::ReadOnly | QFile::Text))
+		{
+			qWarning("Cannot open file %s for reading: %s", qPrintable(styleFile.fileName()), qPrintable(styleFile.errorString()));
+			return;
+		}
+
+		QTextStream textStream(&styleFile);
+		QString styleSheet = textStream.readAll();
+		widget->setStyleSheet(styleSheet);
+	}
+}
